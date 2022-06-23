3b9d570223256525ca58a8953fecd802ec792d91
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-23 17:22:37 +0200

    show pin and button form

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,36 @@ const backend_url = import.meta.env.VITE_BACKEND_URL;
 function App() {
     const [jobs, setJobs] = useState([]);
 
-    useEffect(() => {
-        (async () => {
-            setJobs((await axios.get(backend_url)).data);
-        })();
-    }, []);
+    const getJobs = async () => {
+        setJobs((await axios.get(backend_url)).data);
+    };
+
+    const handleShowButton = async (e) => {
+        e.preventDefault();
+        await getJobs();
+    }
 
     return (
         <div className="App">
             <h1>Jobs</h1>
-            <p className="message">There are {jobs.length} jobs.</p>
-            {jobs.map((job, i) => {
-                return (
-                    <div
-                        className="job"
-                        key={i}
-                        dangerouslySetInnerHTML={{ __html: job.html }}
-                    ></div>
-                );
-            })}
+            <form>
+                PIN: <input type="password" className="thePin" autoFocus />
+                <button onClick={(e) => handleShowButton(e)}>Show</button>
+            </form>
+            {jobs.length > 0 && (
+                <>
+                    <p className="message">There are {jobs.length} jobs.</p>
+                    {jobs.map((job, i) => {
+                        return (
+                            <div
+                                className="job"
+                                key={i}
+                                dangerouslySetInnerHTML={{ __html: job.html }}
+                            ></div>
+                        );
+                    })}
+                </>
+            )}
         </div>
     );
 }
