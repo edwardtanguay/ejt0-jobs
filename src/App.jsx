d459b7dd8a162e41fccae8b7e7678600fc1c63c1
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-23 07:22:09 +0200

    ready for publish

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import './App.css'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.scss';
+
+const url = 'http://localhost:3021/jobs';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Jobs</h1>
-    </div>
-  )
+    const [jobs, setJobs] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            setJobs((await axios.get(url)).data);
+        })();
+    }, []);
+
+    return (
+        <div className="App">
+            <h1>Jobs</h1>
+            <p>There are {jobs.length} jobs.</p>
+        </div>
+    );
 }
 
-export default App
+export default App;
